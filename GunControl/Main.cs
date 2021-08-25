427ffbed9cff49f749e7f382ca7b347a8b8a149a
WithLithum <RelaperCrystal@163.com>
2021-08-25 15:17:25 +0800

    :sparkles: Add feature

---
+++ GunControl/Main.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GTA;
+
+namespace GunControl
+{
+    public class Main : Script
+    {
+        private readonly List<Ped> _swappedPeds = new List<Ped>();
+
+        public Main()
+        {
+            this.Interval = 250;
+            this.Tick += Main_Tick;
+        }
+
+        private void Main_Tick(object sender, EventArgs e)
+        {
+            foreach (var ped in World.GetAllPeds())
+            {
+                Yield();
+                if (ped?.Exists() != true) continue;
+                if (_swappedPeds.Contains(ped)) continue;
+                if (ped.IsDead || ped.IsPlayer) continue;
+
+                Util.SwapNow(ped);
+                Yield();
+                _swappedPeds.Add(ped);
+            }
+
+            Yield();
+            for (var i = 0; i < _swappedPeds.Count; i++)
+            {
+                Yield();
+                if (_swappedPeds[i]?.Exists() != true || _swappedPeds[i].IsDead)
+                {
+                    _swappedPeds.RemoveAt(i);
+                }
+            }
+        }
+    }
+}
---
+++ GunControl/Util.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GTA;
+
+namespace GunControl
+{
+    internal static class Util
+    {
+        internal static bool IsGangMember(this Ped ped)
+        {
+            return ped.RelationshipGroup == "AMBIENT_GANG_LOST" || ped.RelationshipGroup == "AMBIENT_GANG_MEXICAN" || ped.RelationshipGroup == "AMBIENT_GANG_FAMILY"
+                || ped.RelationshipGroup == "AMBIENT_GANG_BALLAS" || ped.RelationshipGroup == "AMBIENT_GANG_MARABUNTE" || ped.RelationshipGroup == "AMBIENT_GANG_CULT"
+                || ped.RelationshipGroup == "AMBIENT_GANG_SALVA" || ped.RelationshipGroup == "AMBIENT_GANG_WEICHENG" || ped.RelationshipGroup == "AMBIENT_GANG_HILLBILLY";
+        }
+
+        internal static bool IsCop(this Ped ped)
+        {
+            return ped.RelationshipGroup == "COP";
+        }
+
+        internal static void SwapNow(Ped ped)
+        {
+            ped.Weapons.RemoveAll();
+            if (ped.IsGangMember())
+            {
+                var randomInt = new Random(new Random().Next()).Next(1, 4);
+                switch (randomInt)
+                {
+                    case 1:
+                        ped.Weapons.Give(WeaponHash.Knife, 1, true, true);
+                        break;
+                    case 2:
+                        ped.Weapons.Give(WeaponHash.Bat, 1, true, true);
+                        break;
+                    case 3:
+                        ped.Weapons.Give(WeaponHash.Bottle, 1, true, true);
+                        break;
+                }
+            }
+            if (ped.IsCop())
+            {
+                ped.Weapons.Give(WeaponHash.Nightstick, 1, false, false);
+            }
+        }
+    }
+}
