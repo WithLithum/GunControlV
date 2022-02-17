WithLithum/GunControlV

commit 2ed6ad2f13d0a56216e3ae58732ec47ce5f8f824

:zap: Calculate gang hashes at startup

GunControl/Main.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using GTA;
 using GunControl.Rules;
 
@@ -20,9 +17,7 @@ namespace GunControl
 
         internal static int ProcessingInterval { get; private set; }
 
-#pragma warning disable S2223 // Non-constant static fields should not be visible
-        internal static string[] GangGroups;
-#pragma warning restore S2223 // Non-constant static fields should not be visible
+        internal static readonly List<int> GangGroups;
 
         public Main()
         {
@@ -42,9 +37,11 @@ namespace GunControl
 
             // cache the values first
             var strings = Settings.GetValue("Misc", "GangGroups", "AMBIENT_GANG_LOST,AMBIENT_GANG_MEXICAN,AMBIENT_GANG_FAMILY,AMBIENT_GANG_BALLAS,AMBIENT_GANG_MARABUNTE,AMBIENT_GANG_CULT,AMBIENT_GANG_SALVA,AMBIENT_GANG_WEICHENG,AMBIENT_GANG_HILLBILLY");
-#pragma warning disable S3010 // Static fields should not be updated in constructors
-            GangGroups = strings.Split(','); // Read gang groups configuration as an enum
-#pragma warning restore S3010 // Static fields should not be updated in constructors
+
+            foreach (var group in strings.Split(','))
+            {
+                GangGroups.Add(Game.GenerateHash(group));
+            }
         }
 
         private void Main_Tick(object sender, EventArgs e)

GunControl/Util.cs
@@ -19,7 +19,6 @@ namespace GunControl
             // The check is simple
             // All it does is compare peds' current group with every single groups defined in INI
 
-            // TODO: Calculate hash at startup so we don't have to calculate every single time
             foreach (var group in Main.GangGroups)
             {
                 if (ped.RelationshipGroup == group) return true;

commit c2d14e329b250d2d98325edf14c7cd764943cc07

:bug: Fix unassigned GangGroups
That was introduced in 2ed6ad2f

GunControl/Main.cs
@@ -17,7 +17,8 @@ namespace GunControl
 
         internal static int ProcessingInterval { get; private set; }
 
-        internal static readonly List<int> GangGroups;
+        internal static readonly List<int> GangGroups
+            = new List<int>();
 
         public Main()
         {